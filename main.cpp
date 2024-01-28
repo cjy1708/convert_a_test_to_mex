b25e775a8f4a519951f44122ca0e6eb727675cf3
jinyangchen <2287497792@qq.com>
2024-01-28 22:59:11 +0800

    优化main关于变量的输入

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 // 自定义头文件
 #include <fiberbundle.h>
 #include <computedispersion.h>
+#include <filesystem>
 
 using namespace std;
 
@@ -43,28 +44,63 @@ public:
                     ) }));
         }
         string inputFilename;
-        {
+        // 判断inputFilename的类型
+        if (inputs[0].getType() == ArrayType::CHAR) {
+            // 如果是char类型,则转换为string
+            CharArray input_filename = inputs[0];
+            inputFilename = input_filename.toAscii();
+        } else if (inputs[0].getType() == ArrayType::MATLAB_STRING) {
             StringArray input_filename = inputs[0];
             inputFilename = matlab::engine::convertUTF16StringToUTF8String(input_filename[0]);
+        } else {
+            // 在matlab上打印错误日志
+            matlabPtr->feval(u"error", 0, std::vector<Array>({ factory.createScalar(
+                    "Error: the input file name should be char or string."
+            ) }));
+        }
+        cout << "inputFilename: " << inputFilename << endl;
+        // 判断对应路径的文件是否存在
+        ifstream f(inputFilename);
+        if (!f.good()) {
+            // 在matlab上打印错误日志
+            matlabPtr->feval(u"error", 0, std::vector<Array>({ factory.createScalar(
+                    "Error: the input file is not exist. \n"
+                    "rerun result this function with no input and two output. \n"
+            ) }));
         }
         string outputFilename;
+        if (inputs[1].getType() == ArrayType::CHAR)
         {
+            // 如果是char类型,则转换为string
+            CharArray output_filename = inputs[1];
+            outputFilename = output_filename.toAscii();
+        }
+        else if (inputs[1].getType() == ArrayType::MATLAB_STRING) {
             StringArray output_filename = inputs[1];
             outputFilename = matlab::engine::convertUTF16StringToUTF8String(output_filename[0]);
+        } else {
+            // 在matlab上打印错误日志
+            matlabPtr->feval(u"error", 0, std::vector<Array>({ factory.createScalar(
+                    "Error: the output file name should be char or string."
+            ) }));
         }
+        // 判断父目录是否存在,不存在则创建
+        string parentPath = outputFilename.substr(0, outputFilename.find_last_of("/\\"));
+        if (!filesystem::exists(parentPath)) {
+            filesystem::create_directories(parentPath);
+        }
+        cout << "outputFilename: " << outputFilename << endl;
         uint scale = inputs[2][0];
+        cout << "scale: " << scale << endl;
         uint numberOfSamplingDirections = inputs[3][0];
+        cout << "numberOfSamplingDirections: " << numberOfSamplingDirections << endl;
         uint tractSubSampling = inputs[4][0];
+        cout << "tractSubSampling: " << tractSubSampling << endl;
         uint fiberPointSubSampling = inputs[5][0];
+        cout << "fiberPointSubSampling: " << fiberPointSubSampling << endl;
 
         fiberbundle myBundle;
         myBundle.ReadFibers(inputFilename);
-        if (myBundle.GetFibers().empty()) {
-            // 在matlab上打印错误日志
-            matlabPtr->feval(u"error", 0, std::vector<Array>({ factory.createScalar(
-                    "Error: the input file is empty. file path is: " + inputFilename + "\n"
-            ) }));
-        }
         computeDispersion(myBundle, scale, numberOfSamplingDirections, outputFilename, tractSubSampling, fiberPointSubSampling);
         // 输出计算完成
 
@@ -110,7 +146,7 @@ public:
         matlab::data::TypedArray<matlab::data::Array> valuesArray = mx[0]["values"];
         for (size_t i{}; i < values.size(); ++i) {
             valuesArray[i] = std::move(values[i]);
-        }
+        }// 将值的vector转为matlab::data::CellArray对象，并赋值给mx [0] ["values"]
         mx[0]["values"] = std::move(valuesArray);
         return mx; // 返回一个matlab::data::Array对象
     }
